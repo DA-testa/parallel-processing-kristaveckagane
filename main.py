84982c02b85704cb7efa8d5ebbdd106fec48eb69
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-03-18 11:13:58 +0000

    test

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    pavedieni=[(0,1) for i in range(n)]
+    for i, j in enumerate(data):
+        sakums = pavedieni[0][0]
+        sakumsIdx = pavedieni[0][1]
+        for k, Idx in pavedieni[1:]:
+            if k < sakums:
+                sakums = k 
+                sakumsIdx = Idx
+        output.append((sakumsIdx, sakums))
+        pavedieni.remove((sakums, sakumsIdx))
+        pavedieni.append((sakums + j, sakumsIdx))
 
     return output
 
@@ -13,8 +24,9 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    n = int(input().strip())
+    m = int(input().strip())
+    data = list(map(int, input().split()))
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
